dee605810896979d90e0f71a4bcbffdf9cc03d50
Aaroh Mankad <aarohmankad@gmail.com>
2017-12-30 12:53:57 -0800

    allow user to change base and quote currencies in redux store

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import { FlatList, View, StatusBar } from 'react-native';
+import { connect } from 'react-redux';
 
 import { ListItem, Separator } from '../components/List';
 import currencies from '../data/currencies';
+import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
 
 class CurrencyList extends Component {
   static propTypes = {
     navigation: propTypes.object,
+    dispatch: propTypes.func,
+    baseCurrency: propTypes.string,
+    quoteCurrency: propTypes.string,
   };
 
-  handlePress = () => {
+  handlePress = currency => {
+    const { type } = this.props.navigation.state.params;
+
+    if (type === 'base') {
+      this.props.dispatch(changeBaseCurrency(currency));
+    } else if (type === 'quote') {
+      this.props.dispatch(changeQuoteCurrency(currency));
+    }
+
     this.props.navigation.goBack(null);
   };
 
-  render() {
+  render = () => {
+    const { type } = this.props.navigation.state.params;
+    const { baseCurrency, quoteCurrency } = this.props;
+    let currency = type === 'base' ? baseCurrency : quoteCurrency;
+
     return (
       <View style={{ flex: 1 }}>
         <StatusBar translucent={false} barStyle="default" />
@@ -24,9 +41,9 @@ class CurrencyList extends Component {
           renderItem={({ item }) => (
             <ListItem
               text={item}
-              selected={item === 'CAD'}
-              visible={item === 'CAD'}
-              onPress={this.handlePress}
+              selected={item === currency}
+              visible={item === currency}
+              onPress={() => this.handlePress(item)}
             />
           )}
           keyExtractor={item => item}
@@ -34,7 +51,16 @@ class CurrencyList extends Component {
         />
       </View>
     );
-  }
+  };
 }
 
-export default CurrencyList;
+const mapStateToProps = state => {
+  const { baseCurrency, quoteCurrency } = state.currencies;
+
+  return {
+    baseCurrency,
+    quoteCurrency,
+  };
+};
+
+export default connect(mapStateToProps)(CurrencyList);
